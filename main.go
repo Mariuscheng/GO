563c5bd2ff692c59dc7dda24988dc41a15a1d76b
Mariuscheng <47233413+Mariuscheng@users.noreply.github.com>
2021-01-18 17:21:41 +0800

    Add files via upload

--- main.go
+++ main.go
@@ -11,24 +11,33 @@ import (
     "bytes"
     // "log"
 )
-
 type Ecr struct {
-    TYPE string
-    CMD string
-    AMT int64
-}  
-    
-
-
+    TYPE string  `json:"TYPE"`
+    CMD string `json:"CMD"`
+    AMT float64 `json:"AMT"`
+    }
 
 func main() {
-
-    Ecr := Ecr{TYPE: "EDC",CMD:"SALE",AMT: 1,}
     
+     var TYPENAME string
+     var CMDNAME string
+     var ECRAMT float64
+
+    fmt.Println("Enter ECR TYPE:")
+    fmt.Scanln(&TYPENAME)
+
+    fmt.Println("Enter ECR CMD:")
+    fmt.Scanln(&CMDNAME)
+
+    fmt.Println("Enter ECR AMT:")
+    fmt.Scanln(&ECRAMT)
+
+    Ecr := &Ecr{TYPE:TYPENAME,CMD:CMDNAME,AMT:ECRAMT}
+
     jsonstring, _ := json.Marshal(Ecr)
     fmt.Print(string(jsonstring)+"\n")
 
-    resp,_ := http.Post("http://172.16.104.91:8080/", "application/json", bytes.NewBuffer(jsonstring))
+    resp,_ := http.Post("http://172.16.104.27:8080/", "application/json", bytes.NewBuffer(jsonstring))
 
     defer resp.Body.Close()
 
